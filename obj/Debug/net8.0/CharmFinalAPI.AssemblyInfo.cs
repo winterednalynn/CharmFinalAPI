e1f6e0488f356540d53985939a5da4b662c3aa2b
winterednalynn <ednalynnnl@gmail.com>
2024-01-03 18:50:04 -0800

    UPDATE

--- obj/Debug/net8.0/CharmFinalAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/CharmFinalAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CharmFinalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f64729390801be1b65ae5e7fe89d70dce586ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("CharmFinalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CharmFinalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
